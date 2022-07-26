294b01b43a42f34183de7d0de1653d1aadf41387
SalmanSaalu <salmanpp05@gmail.com>
2022-07-26 22:34:52 +0530

    upto  User Login Reducer & Action

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,14 +1,11 @@
-from email import message
-from django.http import JsonResponse
+
 from django.shortcuts import render
-from django.http import JsonResponse
-from .products import products
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer,UserSerializer,UserSerializerWithToken
+
+from base.serializers import ProductSerializer,UserSerializer,UserSerializerWithToken
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -50,9 +47,7 @@ def registerUser(request):
     except:
         message={'detail':'User with email already exist'}
         return Response(message,status=status.HTTP_400_BAD_REQUEST)
-# @api_view(['GET'])
-# def getRoutes(request):
-#     return Response('Hello')
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -67,22 +62,3 @@ def getUsers(request):
     users=User.objects.all()
     serializer=UserSerializer(users,many=True)
     return Response(serializer.data)
-
-@api_view(['GET'])
-def getProducts(request):
-    products=Product.objects.all()
-    serializer=ProductSerializer(products,many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request,pk):
-    product=Product.objects.get(_id=pk)
-    serializer=ProductSerializer(product,many=False)
-    return Response(serializer.data)
-    # product=None
-    # for i in products:
-    #     if i['_id']==pk:
-    #         product=i
-            # break
-    # return Response(product)
